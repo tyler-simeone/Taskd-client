5c974b1e35fe0d3dc8d7123955155d1f537ace96
Tyler Simeone <tylersimeone@Tylers-MacBook-Pro.local>
2024-05-08 21:07:54 -0500

    removed old stuff

--- src/components/column/Column.js
+++ src/components/column/Column.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react"
-import DropContainer from "../features/DropContainer/DropContainer";
 import { Task } from "../task/Task";
 import './styles/Column.css';
 
