1aebb85c3c4fe5a9218ea0cd34ef18843c53904e
Tyler Simeone <tylersimeone@Tylers-MacBook-Pro.local>
2024-05-08 21:08:33 -0500

    removed old imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Navigation } from "./components/navigation/Navigation";
 import ApplicationViews from './ApplicationViews'
-// import { CookiesProvider } from 'react-cookie';
-import { ErrorAlert } from './components/alerts/ErrorAlert';
-import { SuccessAlert } from './components/alerts/SuccessAlert';
 import './App.css';
 
 function App() {
