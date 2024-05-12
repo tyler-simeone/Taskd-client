51450650dba4a28992411f1d027bd14f12384e24
Tyler Simeone <tylersimeone@Tylers-MacBook-Pro.local>
2024-05-12 00:51:46 -0500

    Add task form UI now mostly working in modal

--- src/components/task/AddTask.js
+++ src/components/task/AddTask.js
@@ -1,23 +1,39 @@
 import React, { useState } from "react";
 import { PBInput } from "../controls/inputs/PBInput";
+import { PrimaryButton } from "../controls/buttons/PrimaryButton";
+import { TestData } from "../../TestData";
 
 export const AddTask = ({  }) => {
+    const [isSubmitting, setIsSubmitting] = useState(false);
     const [newTask, setNewTask] = useState({
+        taskId: null,
         taskName: "",
         taskDescription: "",
     });
 
     const handleChange = (evt) => {
         const stateToChange = {...newTask};
+
+        var sortedTestTasks = TestData.Columns[0].tasks.sort((a ,b) => a.id - b.id);
+        var mostRecentTask = sortedTestTasks[sortedTestTasks.length - 1];
+        stateToChange.taskId = mostRecentTask.taskId + 1;
+
         stateToChange[evt.target.name] = evt.target.value;
-        console.log("stateToChange: ", stateToChange);
         setNewTask(stateToChange);
     }
 
+    const handleSubmit = () => {
+        setIsSubmitting(true);
+        console.log("Sending data! ", newTask);
+        TestData.Columns[0].tasks.push(newTask);
+        setIsSubmitting(false);
+    }
+
     return (
-        <>
+        <form>
             <PBInput name={"taskName"} label={"Task Name"} handleChange={handleChange} />
             <PBInput name={"taskDescription"} label={"Task Description"} handleChange={handleChange} />
-        </>
+            <PrimaryButton text={"Submit"} handleSubmit={handleSubmit} isSubmitting={isSubmitting} />
+        </ form>
     );
 }
