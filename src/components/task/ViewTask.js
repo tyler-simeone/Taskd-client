aaceb9bd338cd6b67154d5c7ad2c4f91f2bbd586
Tyler Simeone <tylersimeone@Tylers-MacBook-Pro.local>
2024-05-12 22:37:02 -0500

    delete icon added

--- src/components/task/ViewTask.js
+++ src/components/task/ViewTask.js
@@ -1,5 +1,6 @@
 import React from "react";
 import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import "./ViewTask.css"
 
 export const ViewTask = ({ task, openEditTaskModal }) => {
@@ -7,7 +8,11 @@ export const ViewTask = ({ task, openEditTaskModal }) => {
         task !== undefined ? (
             <div className="task-details">
                 <p>{task.taskDescription}</p>
-                <div className="edit-icon" onClick={() => openEditTaskModal(task)}><EditIcon /></div>
+
+                <div className="icon--container">
+                    <div className="edit-icon" onClick={() => openEditTaskModal(task)}><EditIcon /></div>
+                    <div className="delete-icon" onClick={() => openEditTaskModal(task)}><DeleteIcon /></div>
+                </div>
             </div>
         ) : null
     );
