6e9754acae742005646d237593d2f2b4c20d066b
Tyler Simeone <tylersimeone@Tylers-MacBook-Pro.local>
2024-05-15 08:01:09 -0500

    Added new columnsClient.js

--- src/api/columnsClient.js
+++ src/api/columnsClient.js
@@ -1,44 +1,44 @@
 import { responseHandler } from "../util/responseHandler";
 
-export const tasksClient = {
+export const columnsClient = {
     httpHeaders: { "Content-Type": "application/json", "Accept": "application/json" },
-    baseDevURL: "http://localhost:5273",
-    get baseURL() { return `${this.baseDevURL}/api/tasks` }, 
-    getTasks: (columnId) => {
-        return fetch(`${tasksClient.baseURL}?columnId=${columnId}`, {
+    baseDevURL: "http://localhost:5156",
+    get baseURL() { return `${this.baseDevURL}/api/columns` }, 
+    getColumns: (columnId) => {
+        return fetch(`${columnsClient.baseURL}?columnId=${columnId}`, {
                     method: "GET",
-                    headers: tasksClient.httpHeaders,
+                    headers: columnsClient.httpHeaders,
                 }).then(resp => resp.json())
                 .then(resp => responseHandler(resp));
     },
-    getTask: (taskId, userId) => {
-        return fetch(`${tasksClient.baseURL}/${taskId}?userId=${userId}`, {
+    getColumn: (boardId, userId) => {
+        return fetch(`${columnsClient.baseURL}/?boardId=${boardId}&userId=${userId}`, {
                     method: "GET",
-                    headers: tasksClient.httpHeaders,
+                    headers: columnsClient.httpHeaders,
                 }).then(resp => resp.json())
                 .then(resp => responseHandler(resp));
     },
-    createTask: (payload) => {
-        return fetch(`${tasksClient.baseURL}`, {
+    createColumn: (payload) => {
+        return fetch(`${columnsClient.baseURL}`, {
                     method: "POST",
                     body: JSON.stringify(payload),
-                    headers: tasksClient.httpHeaders,
+                    headers: columnsClient.httpHeaders,
                 }).then(resp => resp.json())
                 .then(resp => responseHandler(resp));
     },
-    updateTask: (payload) => {
-        return fetch(`${tasksClient.baseURL}`, {
+    updateColumn: (payload) => {
+        return fetch(`${columnsClient.baseURL}`, {
                     method: "PUT",
                     body: JSON.stringify(payload),
-                    headers: tasksClient.httpHeaders,
+                    headers: columnsClient.httpHeaders,
                 }).then(resp => resp.json())
                 .then(resp => responseHandler(resp));
     },
-    deleteTask: (payload) => {
-        return fetch(`${tasksClient.baseURL}`, {
+    deleteColumn: (payload) => {
+        return fetch(`${columnsClient.baseURL}`, {
                     method: "DELETE",
                     body: JSON.stringify(payload),
-                    headers: tasksClient.httpHeaders,
+                    headers: columnsClient.httpHeaders,
                 }).then(resp => resp.json())
                 .then(resp => responseHandler(resp));
     }
