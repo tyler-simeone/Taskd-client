d1a04cb43b10c046e9345bbbcd85c6c49c954c41
Tyler Simeone <tylersimeone@Tylers-MacBook-Pro.local>
2025-01-01 09:46:52 -0600

    Added tagsClient.js

---
+++ src/api/tagsClient.js
@@ -0,0 +1,125 @@
+import { responseHandler } from "../util/responseHandler";
+
+export const tagsClient = {
+    get headers() { 
+        return (
+            {
+                "Authorization": `Bearer ${JSON.parse(sessionStorage.getItem("jwt"))}`, 
+                "Content-Type": "application/json", 
+                "Accept": "application/json" 
+            }
+        )
+    },
+    baseDevURL: "http://localhost:5280",
+    get baseURL() { return `${this.baseDevURL}/api/tags` }, 
+    // For the list of available tags to add to a task, per board.
+    getTagsByBoardId: async (boardId, userId) => {
+        const resp = await fetch(`${tagsClient.baseURL}?boardId=${boardId}&userId=${userId}`, {
+            method: "GET",
+            headers: tagsClient.headers,
+        });
+        
+        let parsedResp = resp;
+        try {
+            parsedResp = await resp.json();
+        } catch (err) {
+            // swallow error here. just means no response body was returned.
+        }
+
+        return responseHandler(parsedResp);
+    },
+    // Gets all tags that are tied to tasks on the board.
+    getTaskTags: async (boardId, userId) => {
+        const resp = await fetch(`${tagsClient.baseURL}/tasks?boardId=${boardId}&userId=${userId}`, {
+            method: "GET",
+            headers: tagsClient.headers,
+        });
+        
+        let parsedResp = resp;
+        try {
+            parsedResp = await resp.json();
+        } catch (err) {
+            // swallow error here. just means no response body was returned.
+        }
+
+        return responseHandler(parsedResp);
+    },
+    createTag: async (payload) => {
+        const resp = await fetch(`${tagsClient.baseURL}`, {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: tagsClient.headers,
+        });
+
+        let parsedResp = resp;
+        try {
+            parsedResp = await resp.json();
+        } catch (err) {
+            // swallow error here. just means no response body was returned.
+        }
+
+        return responseHandler(parsedResp);
+    },
+    addTagToTask: async (payload) => {
+        const resp = await fetch(`${tagsClient.baseURL}/task`, {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: tagsClient.headers,
+        });
+
+        let parsedResp = resp;
+        try {
+            parsedResp = await resp.json();
+        } catch (err) {
+            // swallow error here. just means no response body was returned.
+        }
+
+        return responseHandler(parsedResp);
+    },
+    // updateTag: async (payload) => {
+    //     const resp = await fetch(`${tagsClient.baseURL}`, {
+    //         method: "PUT",
+    //         body: JSON.stringify(payload),
+    //         headers: tagsClient.headers,
+    //     });
+        
+    //     let parsedResp = resp;
+    //     try {
+    //         parsedResp = await resp.json();
+    //     } catch (err) {
+    //         // swallow error here. just means no response body was returned.
+    //     }
+
+    //     return responseHandler(parsedResp);
+    // },
+    deleteTag: async (tagId, userId) => {
+        const resp = await fetch(`${tagsClient.baseURL}/${tagId}?userId=${userId}`, {
+            method: "DELETE",
+            headers: tagsClient.headers,
+        });
+
+        let parsedResp = resp;
+        try {
+            parsedResp = await resp.json();
+        } catch (err) {
+            // swallow error here. just means no response body was returned.
+        }
+
+        return responseHandler(parsedResp);
+    },
+    deleteTagFromTask: async (taskTagId, userId) => {
+        const resp = await fetch(`${tagsClient.baseURL}/task/?taskTagId=${taskTagId}&userId=${userId}`, {
+            method: "DELETE",
+            headers: tagsClient.headers,
+        });
+
+        let parsedResp = resp;
+        try {
+            parsedResp = await resp.json();
+        } catch (err) {
+            // swallow error here. just means no response body was returned.
+        }
+
+        return responseHandler(parsedResp);
+    }
+  };
