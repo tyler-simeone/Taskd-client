8e790dd4f9c0180286d2aa5789183f1e5bca8eb6
Tyler Simeone <tylersimeone@Tylers-MacBook-Pro.local>
2025-05-10 09:51:02 -0500

    UI updates

--- src/api/tasksClient.js
+++ src/api/tasksClient.js
@@ -11,9 +11,9 @@ export const tasksClient = {
         )
     },
     baseDevURL: "http://localhost:5273",
-    get baseURL() { return `${this.baseDevURL}/api/tasks` }, 
-    getTasks: async (columnId) => {
-        const resp = await fetch(`${tasksClient.baseURL}?columnId=${columnId}`, {
+    get baseURL() { return `${this.baseDevURL}` }, 
+    getTasks: async (boardId, columnId) => {
+        const resp = await fetch(`${tasksClient.baseURL}/board/${boardId}/column/${columnId}`, {
             method: "GET",
             headers: tasksClient.headers,
         });
@@ -29,7 +29,7 @@ export const tasksClient = {
         return responseHandler(parsedResp);
     },
     getTask: async (taskId, userId) => {
-        const resp = await fetch(`${tasksClient.baseURL}/${taskId}?userId=${userId}`, {
+        const resp = await fetch(`${tasksClient.baseURL}/api/Tasks/${taskId}?userId=${userId}`, {
             method: "GET",
             headers: tasksClient.headers,
         });
@@ -45,7 +45,7 @@ export const tasksClient = {
         return responseHandler(parsedResp);
     },
     createTask: async (payload) => {
-        const resp = await fetch(`${tasksClient.baseURL}`, {
+        const resp = await fetch(`${tasksClient.baseURL}/api/Tasks/`, {
             method: "POST",
             body: JSON.stringify(payload),
             headers: tasksClient.headers,
@@ -62,7 +62,7 @@ export const tasksClient = {
         return responseHandler(parsedResp);
     },
     updateTask: async (payload) => {
-        const resp = await fetch(`${tasksClient.baseURL}`, {
+        const resp = await fetch(`${tasksClient.baseURL}/api/Tasks/`, {
             method: "PUT",
             body: JSON.stringify(payload),
             headers: tasksClient.headers,
@@ -79,7 +79,7 @@ export const tasksClient = {
         return responseHandler(parsedResp);
     },
     dropTask: async (payload) => {
-        const resp = await fetch(`${tasksClient.baseURL}/dropped`, {
+        const resp = await fetch(`${tasksClient.baseURL}/api/Tasks/dropped`, {
             method: "PUT",
             body: JSON.stringify(payload),
             headers: tasksClient.headers,
@@ -95,7 +95,7 @@ export const tasksClient = {
         return responseHandler(parsedResp);
     },
     deleteTask: async (taskId, userId) => {
-        const resp = await fetch(`${tasksClient.baseURL}/${taskId}?userId=${userId}`, {
+        const resp = await fetch(`${tasksClient.baseURL}/api/Tasks/${taskId}?userId=${userId}`, {
             method: "DELETE",
             headers: tasksClient.headers,
         });
