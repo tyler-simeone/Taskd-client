5c56f97196409e838b4dd70f8818893f3bff27b6
Tyler Simeone <tylersimeone@tylers-mbp.lan>
2025-07-02 21:52:03 -0500

    Adding ability to edit board name

--- src/controls/icons/TaskdLogo.js
+++ src/controls/icons/TaskdLogo.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export const TaskdLogo = ({ isAuthenticated, boardName, isLink, style }) => {
+    const [isEdit, setIsEdit] = useState(false);
+
+    const handleEdit = () => setIsEdit(!isEdit);
+
+    useEffect(() => {
+        console.log("isEdit: ", isEdit);
+    }, [isEdit])
+
     return (
         <div style={style} className="pb-logo">
             {isLink ? (
-                <Link to={isAuthenticated === true ? "/board" : "/login"}>Task'd</Link>
+                <Link to={isAuthenticated ? "/board" : "/login"}>Task'd</Link>
             ) : (
-                <>
-                    <span>Task'd</span> {boardName && (<><span style={{fontSize: 18, color: "#949292", margin: "0px 6px"}}>/</span> <span style={{fontSize: 18, color: "#949494"}}>{boardName}</span></>)}
-                </>
+                <div style={{display: "flex"}}>
+                    <p>Task'd</p> 
+                    
+                    {boardName && (
+                        <div style={{display: "flex", marginTop: 8, marginLeft: 2, fontSize: 18, color: "#949494"}}>
+                            <p style={{margin: "0px 8px"}}>/</p> 
+                            <p onClick={handleEdit}>{boardName}</p>
+                        </div>
+                    )}
+                </div>
             )}
         </div>
     );
