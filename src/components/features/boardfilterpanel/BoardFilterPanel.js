be06d6461b47d9b33fa4987c1c2e0aea840694d9
Tyler Simeone <tylersimeone@tylers-mbp.lan>
2025-07-08 17:02:06 -0500

    Added new PopoutMenuSelectSearch component to use as the filter dropdown

--- src/components/features/boardfilterpanel/BoardFilterPanel.js
+++ src/components/features/boardfilterpanel/BoardFilterPanel.js
@@ -2,12 +2,20 @@ import React, { useState, useEffect, useContext } from "react";
 import { AppContext } from "../../../AppContextProvider";
 import { FilterIcon } from "../../../controls/icons/FilterIcon";
 import { Select } from "../../../controls/inputs/Select";
+import { PopoutMenuSelectSearch } from "../popoutmenuselectsearch/PopoutMenuSelectSearch";
 import "./BoardFilterPanel.css";
 
 export const BoardFilterPanel = () => {
     const { boardId, setTagFilterCriteria } = useContext(AppContext);
 
     const [showFilters, setShowFilters] = useState(false);
+    const [popoutMenuValues, setPopoutMenuValues] = useState([
+        {
+            name: "editColumn",
+            value: "Edit Column",
+            // callback: () => openEditColumnModal(column.columnId)
+        }
+    ]); 
 
     const handleClick = () => {
         setShowFilters(!showFilters);
@@ -20,10 +28,11 @@ export const BoardFilterPanel = () => {
     return (
         <div className="board-filter-panel--container">
             {showFilters && (
-                <Select 
-                    placeholder={"Filter Tags"}
-                    style={{display: "flex"}}
-                />
+                // <Select 
+                //     placeholder={"Filter Tags"}
+                //     style={{display: "flex"}}
+                // />
+                <PopoutMenuSelectSearch options={popoutMenuValues} />
             )}
 
             <FilterIcon onClick={handleClick} />
---
+++ src/components/features/popoutmenuselectsearch/PopoutMenuSelectSearch.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import TogglePopout from "../popoutmenu/TogglePopout";
+import { Input } from "../../../controls/inputs/Input";
+import "./PopoutMenuSelectSearch.css"
+
+export const PopoutMenuSelectSearch = ({ options, idx }) => {
+
+    const showMoreOptions = (evt) => {
+        const modal = document.getElementById(`more-options-modal-${idx}`);
+
+        const isModalOpen = modal.style.display === 'block';
+
+        // console.log("isModalOpen: ", isModalOpen);
+
+        // close modal if mouse click is not in modal
+        // if (isModalOpen && !modal.contains(evt.target)) {
+        //     modal.style.display = 'none';
+        //     return;
+        // }
+        
+        modal.style.left = `${evt.pageX}px`;
+        modal.style.top = `${evt.pageY}px`;
+        modal.style.display = 'block';
+    };
+    const closeMoreOptions = () => {
+        const modal = document.getElementById(`more-options-modal-${idx}`);
+        modal.style.display = 'none';
+    };
+
+    return (
+        <>
+            <TogglePopout
+                renderButton={({ isOpen }) => (
+                    <Input style={{height: 30, border: "1px solid #e1e1e1"}} />
+                )}
+            >
+                {({ position, ref }) => (
+                    <div 
+                        ref={ref}
+                        style={{
+                            top: position.top,
+                            left: position.left,
+                            position: 'absolute',
+                            backgroundColor: "#fff",
+                            borderRadius: 5.5,
+                            border: "1px solid lightgrey",
+                            boxShadow: "0px 2px 3px 1px rgb(0 0 0 / 20%)"
+                        }}      
+                    >
+                        <ul style={{padding: "16px 0px", margin: "0 auto", width: 188}}>
+                            {options && (
+                                options.map(option => (
+                                    <li className="more-options-li" key={option.name} name={option.name} onClick={option.callback}>
+                                        {option.value}
+                                    </li>
+                                ))
+                            )}
+                        </ul>
+                    </div>
+                )}
+            </TogglePopout>
+        </>
+    );
+}
