20015842d99508c10ce91fd5c9dab25f376fddbc
Tyler Simeone <tylersimeone@tylers-mbp.lan>
2025-07-15 21:00:16 -0500

    Tags filter mostly working now... Will clean it up next

--- src/components/features/boardfilterpanel/BoardFilterPanel.js
+++ src/components/features/boardfilterpanel/BoardFilterPanel.js
@@ -18,6 +18,7 @@ export const BoardFilterPanel = () => {
     const [popoutMenuValues, setPopoutMenuValues] = useState();
     const [filterCriteria, setFilterCriteria] = useState();
     const [tagFilterPlaceholder, setTagFilterPlaceholder] = useState();
+    const [sessionFilterCriteria, setSessionFilterCriteria] = useState();
 
     const handleClick = () => {
         setShowFilters(!showFilters);
@@ -27,48 +28,53 @@ export const BoardFilterPanel = () => {
         const tagFilterName = filterCriteria.sort((a, b) => a.tagName.localeCompare(b.tagName))[0]?.tagName;
         const additionalTagFilterCount = filterCriteria.length-1;
         
-        if (additionalTagFilterCount !== 0)
-            setTagFilterPlaceholder(`${tagFilterName} (+${additionalTagFilterCount})`);
-        
-        setTagFilterPlaceholder(`${tagFilterName}`);
+        if (tagFilterName) {
+            if (additionalTagFilterCount !== 0)
+                setTagFilterPlaceholder(`${tagFilterName} (+${additionalTagFilterCount})`);
+            
+            setTagFilterPlaceholder(`${tagFilterName}`);
+        } else {
+            setTagFilterPlaceholder("No tags found");
+        }
     }
     
-    const handleAddToTagFilterCriteria = (tagId, tagName) => {
+    const handleToggleTagInFilterCriteria = (tagId, tagName) => {
         let persistedFilterCriteria = JSON.parse(sessionStorage.getItem("filterCriteria"));
         if (!persistedFilterCriteria || !Array.isArray(persistedFilterCriteria))
             persistedFilterCriteria = [];
 
         const updatedFilterCriteria = [...persistedFilterCriteria];
+        console.log("pre--updatedFilterCriteria: ", updatedFilterCriteria);
         if (!updatedFilterCriteria.find(c => c.tagId === tagId)) {
             const criteria = {
                 tagId: tagId,
                 tagName: tagName
             };
             updatedFilterCriteria.push(criteria);
-        }
-
-        sessionStorage.setItem("filterCriteria", JSON.stringify(updatedFilterCriteria));
-        setFilterCriteria(updatedFilterCriteria);
-        setBoardHasChanged(true);
-        handleRerender();
-    };
-    
-    const handleRemoveTagFromFilterCriteria = (tagId) => {
-        console.log("handleRemoveTagFromFilterCriteria: ", handleRemoveTagFromFilterCriteria);
-        let persistedFilterCriteria = JSON.parse(sessionStorage.getItem("filterCriteria"));
-        if (!persistedFilterCriteria || !Array.isArray(persistedFilterCriteria))
-            persistedFilterCriteria = [];
-
-        const updatedFilterCriteria = [...persistedFilterCriteria];
-        if (updatedFilterCriteria.find(c => c.tagId === tagId)) {
+            console.log("post--updatedFilterCriteria: ", updatedFilterCriteria);
+        } else {
             const existingTagIdx = updatedFilterCriteria.indexOf(updatedFilterCriteria.find(c => c.tagId === tagId));
-            updatedFilterCriteria = updatedFilterCriteria.splice(existingTagIdx, 1);
+            console.log("existingTagIdx: ", existingTagIdx);
+            updatedFilterCriteria.splice(existingTagIdx, 1);
+            console.log("post--updatedFilterCriteria: ", updatedFilterCriteria);
         }
 
         sessionStorage.setItem("filterCriteria", JSON.stringify(updatedFilterCriteria));
         setFilterCriteria(updatedFilterCriteria);
-        setBoardHasChanged(true);
-        handleRerender();
+        
+        if (updatedFilterCriteria.length !== 0) {
+            sessionStorage.setItem("filterCriteria", JSON.stringify(updatedFilterCriteria));
+            setSessionFilterCriteria(updatedFilterCriteria);
+            setFilterCriteria(updatedFilterCriteria);
+            setBoardHasChanged(true);
+            handleRerender();
+        } else {
+            sessionStorage.removeItem("filterCriteria");
+            setSessionFilterCriteria();
+            setFilterCriteria();
+            setBoardHasChanged(true);
+            handleRerender();
+        }
     };
 
     const loadTags = () => {
@@ -79,18 +85,7 @@ export const BoardFilterPanel = () => {
                     id: r.tagId,
                     value: r.tagName,
                     name: r.tagName,
-                    onClick: () => {
-                        console.log("filterCriteria: ", filterCriteria);
-                        if (filterCriteria && filterCriteria.length > 0) {
-                            if (!filterCriteria.find(c => c.tagId === r.tagId)) {
-                                console.log("heyyo");
-                                handleAddToTagFilterCriteria(r.tagId, r.tagName);
-                            } else {
-                                handleRemoveTagFromFilterCriteria(r.tagId);
-                            }
-                        } else
-                            handleAddToTagFilterCriteria(r.tagId, r.tagName);
-                    }
+                    onClick: () => handleToggleTagInFilterCriteria(r.tagId, r.tagName)
                 }));
                 popoutValues.sort((a, b) => a.value.localeCompare(b.value));
                 setPopoutMenuValues(popoutValues);
@@ -106,10 +101,14 @@ export const BoardFilterPanel = () => {
         } else {
             if (!filterCriteria) {
                 console.log("hi1");
-                const sessionFilterCriteria = JSON.parse(sessionStorage.getItem("filterCriteria"));
                 if (sessionFilterCriteria) {
-                    console.log("hi2");
-                    setFilterCriteria(sessionFilterCriteria);
+                    if (sessionFilterCriteria.length === 0) {
+                        // setSessionFilterCriteria();
+                        setFilterCriteria();
+                    } else {
+                        console.log("hi2");
+                        setFilterCriteria(sessionFilterCriteria);
+                    }
                 }
             }
             else
@@ -120,12 +119,24 @@ export const BoardFilterPanel = () => {
     useEffect(() => {
         console.log("boardIdHasChanged: ", boardIdHasChanged);
 
+        if (!sessionFilterCriteria) {
+            const storageCriteria = JSON.parse(sessionStorage.getItem("filterCriteria"));
+            if (storageCriteria) {
+                if (storageCriteria.length > 0) {
+                    setSessionFilterCriteria(storageCriteria);
+                } else {
+                    sessionStorage.removeItem("filterCriteria");
+                    setSessionFilterCriteria();
+                }
+            }
+        }
+
         loadFilters();
 
         if (boardId && (boardIdHasChanged || (!popoutMenuValues || popoutMenuValues.length === 0))) {
             loadTags();
         }
-    }, [popoutMenuValues, filterCriteria, boardIdHasChanged])
+    }, [popoutMenuValues, filterCriteria, boardIdHasChanged, sessionFilterCriteria])
 
     return (
         <div className="board-filter-panel--container">
