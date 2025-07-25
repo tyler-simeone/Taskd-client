a955c3e2676cc856db443a005e7e6ff30e1bccb1
Tyler Simeone <tylersimeone@tylers-mbp.lan>
2025-07-25 07:17:53 -0500

    UI updates

--- src/controls/icons/TaskdLogo.js
+++ src/controls/icons/TaskdLogo.js
@@ -64,7 +64,8 @@ export const TaskdLogo = ({ isAuthenticated, boardName, isLink, style }) => {
                     
                     {/* Editable Board Name */}
                     {boardName && (
-                        <div style={{display: "flex", marginTop: 8, marginLeft: 2, fontSize: 17.5, lineHeight: "22px", color: "#a0a0a0"}}>
+                        // <div style={{display: "flex", marginTop: 8.5, marginLeft: 2, fontSize: 17.5, lineHeight: "22px", color: "#a0a0a0"}}>
+                        <div style={{display: "flex", marginTop: 8.5, marginLeft: 2, fontSize: 17.5, lineHeight: "22px", color: "#9c9c9c"}}>
                             <p style={{margin: "0px 8px", marginBottom: 0}}>/</p>
                             {!isEdit ? (
                                 <p style={{marginBottom: 0}} onClick={handleEdit}>{boardName}</p>
