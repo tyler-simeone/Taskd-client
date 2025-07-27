6525069bc55ef3a20480db4c5fd2ba7bd42145d4
Tyler Simeone <tylersimeone@tylers-mbp.lan>
2025-07-27 12:06:26 -0500

    UI updates

--- src/components/navigation/NavigationSelect.js
+++ src/components/navigation/NavigationSelect.js
@@ -29,7 +29,7 @@ export const NavigationSelect = ({ selectedValue, options }) => {
                 handleSelectChange={handleSelectChange}
                 value={selectedValue}
                 placeholder={"No boards yet"}
-                style={{border: "1.5px solid rgb(0 0 0 / 12%)", padding: 8.5}}
+                style={{border: "1.5px solid rgb(0 0 0 / 12%)", padding: 8}}
             />
         </div>
     );
